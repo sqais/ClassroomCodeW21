1717a1b136143ee8a431494d1874a67851b451ff
Munima <Munima@LAPTOP-HG73CSET>
2021-01-24 20:47:26 -0700

    Multiple package adde

---
+++ src/enumpack/TestEnum.java
@@ -0,0 +1,19 @@
+package enumpack;
+
+//A simple enum example where enum is declared 
+//outside any class (Note enum keyword instead of 
+//class keyword) 
+enum Color 
+{ 
+ RED, GREEN, BLUE; 
+} 
+
+public class TestEnum 
+{ 
+ // Driver method 
+ public static void main(String[] args) 
+ { 
+     Color c1 = Color.RED; 
+     System.out.println(c1); 
+ } 
+}
